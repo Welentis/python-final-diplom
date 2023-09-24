50716a98ca6d2ee0dd1b112857ccb82fff178a0d
Sengshi <asket2013@yandex.ru>
2023-09-24 23:34:01 +1000

    Update check token

--- orders/backend/urls.py
+++ orders/backend/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import *
 
 
 urlpatterns = [
     path('update/<str:file_name>', PartnerUpdate.as_view(), name='update_products'),
     path('register', UserRegisterView.as_view(), name='register'),
-    path('login', UserLoginView.as_view(), name='login'),
+    path('login', obtain_auth_token, name='login'),
     path('products', ProductView.as_view(), name='products'),
     path('products/<int:id>', ProductDetailView.as_view(), name='products'),
     path('order', OrderView.as_view(), name='order'),
