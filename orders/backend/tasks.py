7b3edef46597526919ecfe43501f1adfb7296920
Denis Kel <asket2013@ya.ru>
2023-11-22 23:44:35 +1000

    update for docker

--- orders/backend/tasks.py
+++ orders/backend/tasks.py
@@ -7,10 +7,10 @@ from .models import ConfirmEmailToken, User
 from celery import shared_task
 
 
-@shared_task
+@shared_task()
 def new_user_registered_signal(user_id, **kwargs):
     """
-    отправляем письмо с подтрердждением почты
+    отправляем письмо с подтверждением почты
     """
     # send an e-mail to the user
     # token, _ = ConfirmEmailToken.objects.create(user_id=user_id)
@@ -27,7 +27,7 @@ def new_user_registered_signal(user_id, **kwargs):
     message.send()
 
 
-@shared_task
+@shared_task()
 def new_order_signal(user_id, **kwargs):
     # send an e-mail to the user
     user = User.objects.get(id=user_id)
--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -192,4 +192,7 @@ AUTHENTICATION_BACKENDS = (
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
